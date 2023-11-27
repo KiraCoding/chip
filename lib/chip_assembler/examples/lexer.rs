3abeb44d83f2c83cec714f37164391ddf432ae27
KiraCoding <38864051+KiraCoding@users.noreply.github.com>
2023-11-27 08:50:00 +0100

    Add lexer example

---
+++ lib/chip_assembler/examples/lexer.rs
@@ -0,0 +1,18 @@
+use chip_assembler::lexer::Lexer;
+
+fn main() {
+    let lexer = Lexer::from(
+        r#"
+          ; Looping program that does nothing
+
+          cls        ; Clear screen as NOP
+          se  v0, v1
+          se  v0, 20
+          sne v0, v1
+          sne v0, 20
+          jmp 0x200  ; Jump back to the loop
+        "#,
+    );
+
+    println!("{:?}", lexer.collect::<Vec<_>>())
+}
